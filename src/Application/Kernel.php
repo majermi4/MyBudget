c96f431ac2a609c81848a272962f123fd5d6c19d
Michal Majer <michal.majer@rohea.com>
2018-12-25 21:01:55 +0100

    Change app namespace, move Kernel class.

--- src/Application/Kernel.php
+++ src/Application/Kernel.php
@@ -1,6 +1,7 @@
 <?php
+declare(strict_types=1);
 
-namespace App;
+namespace MyBudget\Application;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
